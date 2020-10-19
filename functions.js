8760183a626f0c5a2031ca39daa4daa134ebba31
Luisnovaa <71041813+Luisnovaa@users.noreply.github.com>
2020-10-18 20:46:26 -0400

    update

--- functions.js
+++ functions.js
@@ -1,16 +1,16 @@
 class  Bill{
     constructor() {
         this.item = [];
-        this.price = [];
-        this.url = [];
+        this.cost = [];
+        this.image = [];
       }
     addItem() {
       var item = document.getElementById("item").value;
-      var price = parseFloat(document.getElementById("price").value);
-      var url = document.getElementById("url").value;
+      var cost = parseFloat(document.getElementById("cost").value);
+      var image = document.getElementById("image").value;
         this.item.push(item);
-          this.price.push(price);
-          this.url.push(url);
+          this.cost.push(cost);
+          this.image.push(image);
         this.add();
       }
       add(){
@@ -18,26 +18,26 @@ class  Bill{
         var len = this.item.length;
         text = "<table><tr><th colspan='4'> Bill </th></tr>";
         text += "<tr><td> item </td>";
-        text += "<td>price</td>";
+        text += "<td>cost</td>";
         text += "<td>image</td>";
         text += "<td>delete Item</td></tr>";
 
         for (i=0; i < len; i++) {
           var item = this.item[i];
-          var price = this.price[i];
-          var url = this.url[i];
+          var cost = this.cost[i];
+          var image = this.image[i];
           var button = '<button type="button" onclick="newList.deleteItem('+ i +');">delete item</button>';
           text += "<tr><td>" + item + "</td>";
-          text += "<td>" + "$" + price + "</td>";
-          text += "<td>" + "<img src=" + url + " alt='alternatetext' style='width:100px;height:100px;'>" + "</td>";
+          text += "<td>" + "$" + cost + "</td>";
+          text += "<td>" + "<img src=" + image + " alt='alternatetext' style='width:100px;height:100px;'>" + "</td>";
           text += "<td>" + button + "</td></tr>";
         }
         var j;
         var sum = 0;
-        var len2 =  this.price.length;
+        var len2 =  this.cost.length;
         for (j = 0; j < len2; j++) {
-             price = this.price[j];
-             sum += price;
+             cost = this.cost[j];
+             sum += cost;
         }
         var vat = sum * 0.15;
         var total = sum + vat;
@@ -55,18 +55,18 @@ class  Bill{
 
     eraseAll() {
       var lenitem = this.item.length;
-      var lenprice = this.price.length;
-      var lenurl = this.url.length;
+      var lencost = this.cost.length;
+      var lenimage = this.image.length;
       this.item.splice(0, lenitem);
-      this.price.splice(0, lenprice);
-      this.url.splice(0, lenurl);
+      this.cost.splice(0, lencost);
+      this.image.splice(0, lenimage);
       console.log(this.item.length);
       this.add();
       }
     deleteItem(x) {
     this.item.splice(x, 1);
-    this.price.splice(x, 1);
-    this.url.splice(x, 1);
+    this.cost.splice(x, 1);
+    this.image.splice(x, 1);
     console.log(x);
     this.add();
         }
